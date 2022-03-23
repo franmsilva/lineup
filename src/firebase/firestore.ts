f741f01e6210795fee79f3657c96e7763147a3e2
Francisco Silva <fmsilva1996@gmail.com>
2022-03-23 21:00:26 +0000

    chore: reset compromised compromised GCP credentials

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from './firebase-admin-service-account.json';
+import serviceAccount from './firebase-adminsdk.json';
 
 if (!getApps().length) {
   initializeApp({
