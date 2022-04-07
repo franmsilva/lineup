49289d4dfd928870f9fcfaea3339c19466692299
Francisco Silva <fmsilva1996@gmail.com>
2022-04-07 14:58:32 +0100

    refactor: getAllUsers, getUserById and updateUserById to use Prisma

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,36 +1,51 @@
 import { NextApiHandler } from 'next';
-import db from '@src/firebase/firestore';
-import { handleFirebasePromise, isOk } from '@src/utils/promises';
-
-const usersRef = db.collection('users');
+import prisma from '@src/services/prisma';
 
 const getUserById: NextApiHandler = async (req, res) => {
   const userId = req.query.id as string;
 
-  const result = await handleFirebasePromise(usersRef.doc(userId).get());
-
-  if (!isOk(result)) {
-    res.status(401).json(result.err);
-  } else {
-    res.status(200).json({
-      uid: result.val.id,
-      ...result.val.data(),
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      rejectOnNotFound: true,
     });
+
+    res.status(200).json(user);
+  } catch (err: any) {
+    if (err.name === 'NotFoundError') {
+      res.status(404).json({
+        message: `User not found! ID: ${userId}`,
+      });
+    } else {
+      res.status(500).json(err);
+    }
   }
 };
 
 const updateUserById: NextApiHandler = async (req, res) => {
   const userId = req.query.id as string;
-  const fieldsToUpdate = req.body;
 
-  const result = await handleFirebasePromise(
-    usersRef.doc(userId).update(fieldsToUpdate)
-  );
+  try {
+    const fieldsToUpdate = req.body;
 
-  if (!isOk(result)) {
-    res.status(401).json(result.err);
-  } else {
-    res.send(200);
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: fieldsToUpdate,
+    });
+
+    res.send(204);
+  } catch (err: any) {
+    if (err.code === 'P2025') {
+      res.status(404).json({
+        message: `User not found! ID: ${userId}`,
+      });
+    } else {
+      res.status(500).json(err);
+    }
   }
 };
 
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,9 +1,5 @@
 import { NextApiHandler } from 'next';
-import db from '@src/firebase/firestore';
-import { handleFirebasePromise, isOk } from '@src/utils/promises';
 import prisma from '@src/services/prisma';
-
-const usersRef = db.collection('users');
 
 const createUser: NextApiHandler = async (req, res) => {
   try {
@@ -20,17 +16,16 @@ const createUser: NextApiHandler = async (req, res) => {
 };
 
 const getAllUsers: NextApiHandler = async (req, res) => {
-  const result = await handleFirebasePromise(usersRef.get());
+  try {
+    const users = await prisma.user.findMany();
 
-  if (!isOk(result)) {
-    res.status(500).json(result.err);
-  } else {
-    res.status(200).json(
-      result.val.docs.map((doc) => ({
-        uid: doc.id,
-        ...doc.data(),
-      }))
-    );
+    if (!users) {
+      throw new Error('No users found!');
+    }
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
